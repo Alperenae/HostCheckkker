0371e96ebba1a8f4d6119a3aae57991a1d9bb67d
Alperen <alperen.ergel@yandex.com>
2020-04-18 02:07:33 +0300

    Add files via upload

---
+++ request.py
@@ -0,0 +1,14 @@
+import requests
+# This Is a Basic http request Script. :)
+url = input("Enter The Url: ")
+if url == "" :
+    print("-----------------")
+    print ("ERROR!")
+    print("-----------------")
+#---------------------------
+r = requests.get(url)
+print ("------------------------------------------------------")
+print ("| Response:",r.status_code)
+print ("------------------------------------------------------")
+print ("| Header:",r.headers)
+print ("------------------------------------------------------")
