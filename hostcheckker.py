0987831122cb54af4ab63910ac526cb9c5df94bd
Alperen <alperen.ergel@yandex.com>
2020-05-06 00:05:41 +0300

    Add files via upload

---
+++ hostcheckker.py
@@ -0,0 +1,31 @@
+import argparse
+import requests
+import time
+
+# Colors
+a = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+
+parser = argparse.ArgumentParser('', 'python3 test.py -u --url')    
+parser.add_argument('-u', '--url', type=str, required=True, help='Target')
+args = parser.parse_args()
+
+print(WARNING + "\n [*] Respons is Waiting" + ENDC)
+r = requests.get(args.url)
+
+if r.status_code == 200:
+    time.sleep(3)
+    print ("\n --------------")
+    print (OKGREEN + "\n Host Up Yey!" + ENDC)
+    print ("")
+    print ("--------------")
+
+else:
+    print( FAIL + "\n Host Down Shitt !" + ENDC)
